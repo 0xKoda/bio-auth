0be9263ebbe00e180df0000bd805dc4469f46800
0xKoda <koda_0x@protonmail.com>
2023-01-11 08:50:26 +1000

    fix test

--- test/TestBehaviouralBiometricAuth.t.sol
+++ test/TestBehaviouralBiometricAuth.t.sol
@@ -1,27 +1,27 @@
 pragma solidity ^0.8.0;
 import "../src/BehaviouralBiometricAuth.sol";
-import "ds-test/test.sol";
+import "forge-std/Test.sol";
 
-contract TestBehaviouralBiometricAuth {
+contract TestBehaviouralBiometricAuth is Test {
     BehaviouralBiometricAuth auth;
 
 bytes32[]  data = [    bytes32(0x0123456789012345678901234567890123456789012345678901234567890123),    bytes32(0x0123456789012345678901234567890123456789012345678901234567890124),    bytes32(0x0123456789012345678901234567890123456789012345678901234567890125)];
 
-    constructor() {
+    function setUp() public {
         auth = new BehaviouralBiometricAuth(
             msg.sender,
             data,
 
-            2
+            3
         );
     }
 
     function testAuthenticate() public {
         // Test successful authentication
-        assert(auth.authenticate(data) == true);
+        assert(auth.authenticate(data));
 
         // Test unsuccessful authentication
         data[2] = bytes32(0x0123456789012345678901234567890123456789012345678901234567890126);
-        assert(auth.authenticate(data) == false);
+        assert(!auth.authenticate(data));
     }
 }
